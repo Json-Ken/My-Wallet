0b883e247427a65f34b661a534d8df60a4c619ff
Json <jsonken2@gmail.com>
2023-05-18 06:20:54 +0300

    initial commit

--- app/src/main/java/com/example/mywallet/LoginActivity.kt
+++ app/src/main/java/com/example/mywallet/LoginActivity.kt
@@ -1,11 +1,53 @@
 package com.example.mywallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var tvRedirectSignUp: TextView
+    private lateinit var etEmail: EditText
+    private lateinit var etPass: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        tvRedirectSignUp = findViewById(R.id.txt_Tvdirects)
+        btnLogin = findViewById(R.id.buttonLogin)
+        etEmail = findViewById(R.id.editTextEmail)
+        etPass = findViewById(R.id.editTextPassword)
+        auth = FirebaseAuth.getInstance()
+
+        btnLogin.setOnClickListener {
+            login()
+        }
+        btnLogin.setOnClickListener {
+            val intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        tvRedirectSignUp.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun login() {
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Successfully LoggedIn", Toast.LENGTH_SHORT).show()
+            } else
+                Toast.makeText(this, "Log In failed ", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/mywallet/MainActivity.kt
+++ app/src/main/java/com/example/mywallet/MainActivity.kt
@@ -1,33 +1,36 @@
 package com.example.mywallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.mywallet.Adapter.ActivityAdapter
+import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: ActivityAdapter
+
+class MainActivity() : AppCompatActivity() {
+    private lateinit var Send: ImageView
+    private lateinit var Receive: ImageView
+    private lateinit var Buy: ImageView
+    private lateinit var Home: ImageView
+    private lateinit var Profile: ImageView
+    private lateinit var Wallet: ImageView
+    private lateinit var Settings: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        recyclerView = findViewById(R.id.recyclerViewActivities)
-        adapter = ActivityAdapter(getActivityData()) // Replace with your own data source
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
-    }
 
-    // Replace this function with your own logic to fetch activity data
-    private fun getActivityData(): List<Activity> {
-        // Example data
-        return listOf(
-            Activity("Payment", "Make a payment", R.drawable.ic_payment),
-            Activity("Transfer", "Transfer funds", R.drawable.ic_transfer),
-            Activity("Deposit", "Deposit money", R.drawable.ic_deposit),
-            // Add more activities as needed
-        )
+        Send.findViewById<ImageView>(R.id.img_send)
+        Receive.findViewById<ImageView>(R.id.img_receive)
+        Buy.findViewById<ImageView>(R.id.img_buy)
+        Home.findViewById<ImageView>(R.id.img_btn1)
+        Profile.findViewById<ImageView>(R.id.img_btn2)
+        Wallet.findViewById<ImageView>(R.id.img_btn3)
+        Settings.findViewById<ImageView>(R.id.img_btn4)
+
+        Profile.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
-
-data class Activity(val title: String, val description: String, val iconRes: Int)
